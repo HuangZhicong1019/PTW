008169d764dfa86ff58f41dfb8a64b4c163fedb3
HuangZhicong1019 <zhicong.huang@insa-lyon.fr>
2020-05-25 00:37:51 +0800

    Problème Login

---
+++ Printtheworld1.2-master/Printtheworld1.2-master/app/src/main/java/fr/insalyon/painttheworldapp/Navigation_drawer/Fragment_second.java
@@ -0,0 +1,56 @@
+package fr.insalyon.painttheworldapp.Navigation_drawer;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.insalyon.painttheworldapp.R;
+
+public class Fragment_second extends Fragment {
+    private GridView grid_view;
+    private SimpleAdapter mAdapter;
+    private List<Map<String, Object>> data;
+    File[] img;
+    File file = new File(Environment.getExternalStorageDirectory(),"ScreenShot");
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        img = file.listFiles();
+        View root = inflater.inflate(R.layout.fragment_second, container, false);
+
+        grid_view = root.findViewById(R.id.grid_view);
+
+        data = new ArrayList<Map<String, Object>>();
+        mAdapter = new SimpleAdapter(getContext(),getdata(),
+                R.layout.grid_view,new String[]{"img"},new int[]{R.id.grid});
+        grid_view.setAdapter(mAdapter);
+        return root;
+    }
+
+    private List<Map<String,Object>> getdata(){
+        for (int i=0; i<img.length; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("img", img[i]);
+            map.put("txt", "1");
+            data.add(map);
+        }
+        return data;
+    }
+}
+
+
